c4bf62d1065c132ba02d148cea16fddc30a44073
Kaito Sugimura <sugimurakaito@gmail.com>
2023-07-10 03:07:12 -0600

    remove opacity transition

--- src/Pages/Home/WebsiteProjects/WebsiteProjects.jsx
+++ src/Pages/Home/WebsiteProjects/WebsiteProjects.jsx
@@ -202,7 +202,7 @@ export default function WebsiteProjects() {
                   zIndex: `${Math.round(-(getAbsPosIndex(index) * 10))}`,
                   transition: mouseIsDown.current
                     ? "width 0.3s ease-in-out, height 0.3s ease-in-out"
-                    : `z-index 0.3s ease-in-out, opacity 3s ease-in-out, transform 0.3s ease-in-out, width 0.3s ease-in-out, height 0.3s ease-in-out`,
+                    : `z-index 0.3s ease-in-out, transform 0.3s ease-in-out, width 0.3s ease-in-out, height 0.3s ease-in-out`,
                   transform:
                     selectedView && getPosIndex(index) == 0
                       ? `translate(calc(-50% - ${
